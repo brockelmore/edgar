73465993243340ad871ea5a507e0146d86348e4f
brockelmore <31553173+brockelmore@users.noreply.github.com>
2019-02-06 16:26:12 -0500

    Update parser.go

--- parser.go
+++ parser.go
@@ -369,9 +369,11 @@ func collectDataTags(page io.Reader) map[string]string {
 //   	})
 	doc.Find(".report tbody tr").Each(func(i int, s3 *goquery.Selection) {
 		var text string
+		
 		s3.Find("a").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the band and title
 			text = s.Text()
+			dataTable[text] = make(map[string]interface{})
 			link, _ := s.Attr("onclick")
 			if link[:23] == "top.Show.showAR( this, " {
 				link = link[15:len(link)-1]
