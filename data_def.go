cf4e94e59606586bf8e3f5a40d447ebd0a9c2fd1
brockelmore <31553173+brockelmore@users.noreply.github.com>
2019-02-11 21:52:14 -0500

    Update data_def.go

--- data_def.go
+++ data_def.go
@@ -198,7 +198,8 @@ func setData(fr *financialReport,
 	finType finDataType,
 	val string,
 	scale map[scaleEntity]scaleFactor) error {
-
+	log.Print("scale is: ")
+	log.Println(scale)
 	setter := func(data interface{},
 		finType finDataType,
 		val string,
