d62688a0c2def1f3d5e3e77d41eb8cc5d6e425bc
brockelmore <31553173+brockelmore@users.noreply.github.com>
2019-02-15 20:01:31 -0500

    Update folder.go

--- folder.go
+++ folder.go
@@ -19,7 +19,7 @@ type company struct {
 func (c company) String() string {
 	data, err := json.MarshalIndent(c, "", "    ")
 	if err != nil {
-		log.Println(data)
+		log.Println(err.Error())
 		log.Fatal("Error marshaling Company data")
 	}
 	return string(data)
